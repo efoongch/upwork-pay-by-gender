97497ddad622d0eee2d9e6acdbb179fcb91f63d6
Eureka Foong <eureka.foong@gmail.com>
2017-10-25 10:03:41 -0500

    added script to find average bill rate by gender

--- analyze_workers_from_postgres.py
+++ analyze_workers_from_postgres.py
@@ -19,114 +19,68 @@ class DatabaseAnalyzer:
         self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         psycopg2.extensions.register_adapter(dict, psycopg2.extras.Json)
         self.user_count = 0
-        self.user_ids = []
         self.worker_list = []
+        self.male_list = []
+        self.female_list = []
+        self.unknown_list = []
+        self.problem_list = []
         self.detector = GenderDetector('us')
         self.female_detector_count = 0
         self.male_detector_count = 0
         self.unknown_detector_count = 0
         self.problem_detector_count = 0
         self.detector_rate = 0
+        self.male_average_wage = 0
+        self.female_average_wage = 0
+        self.unknown_average_wage = 0
+        self.problem_average_wage = 0
 
     def identify_gender(self):
         self.cur.execute("SELECT detailed_info FROM upwork_unitedstates_webdev_2017_10_23;")
         for user in self.cur:
             worker_id = user[0]["ciphertext"]
             first_name = user[0]["dev_first_name"]
+            bill_rate = user[0]["dev_bill_rate"]
             gender_by_detector = "none"
-            duplicate = ""
+
             try:
                 print "This is worker {0} whose name is {1}".format(worker_id, first_name)
             except:
                 print "This is worker {0}".format(worker_id)
 
-            # Just double checking that there are no duplicates
-            if worker_id in self.user_ids:
-                duplicate = " and is a duplicate"
-
-            if worker_id not in self.user_ids:
-                duplicate = ""
-
             self.user_count += 1
-            self.user_ids.append(worker_id)
 
-            # Using gender_detector package
-
+            # Using gender_detector package to detect gender
             try:
                 gender_by_detector = self.detector.guess(first_name)
             except:
                 gender_by_detector = "problem"
 
-            self.worker_list.append([first_name, gender_by_detector])
+            self.worker_list.append([first_name, gender_by_detector, bill_rate])
 
-            try:
-                print "Number " + str(self.user_count) + " is {0}".format(first_name) + " with id number {0}".format(worker_id) + "who is {0} (detector)".format(gender_by_detector) + duplicate
-            except:
-                print "Number " + str(self.user_count) + " with id number {0}".format(worker_id) + "who is {0} (detector)".format(gender_by_detector) + duplicate
-
-        # Comparing the effectivness of approaches to identifying gender
+        # Adding bill rate to respective lists
         for i in self.worker_list:
             if i[1] == "male":
-                self.male_detector_count += 1
+                self.male_list.append(i[2])
             elif i[1] == "female":
-                self.female_detector_count += 1
+                self.female_list.append(i[2])
             elif i[1] == "unknown":
-                self.unknown_detector_count += 1
+                self.unknown_list.append(i[2])
             elif i[1] == "problem":
-                self.problem_detector_count += 1
+                self.problem_list.append(i[2])
 
-        self.detector_rate = (self.male_detector_count + self.female_detector_count) / float(self.male_detector_count + self.female_detector_count + self.unknown_detector_count + self.problem_detector_count) * 100
+        self.detector_rate = (self.male_detector_count + self.female_detector_count) / float(self.user_count) * 100
+        self.male_average_wage = numpy.mean(self.male_list)
+        self.female_average_wage = numpy.mean(self.female_list)
+        self.unknown_average_wage = numpy.mean(self.unknown_list)
+        self.problem_average_wage = numpy.mean(self.problem_list)
 
         print "Using gender_detector package: Male: {0}, Female: {1}, Unknown: {2}, Problem: {3}. ".format(self.male_detector_count, self.female_detector_count, self.unknown_detector_count, self.problem_detector_count) + "This is the rate of identifying gender: {0}%".format(self.detector_rate)
         print "This is our final user count: {0}".format(self.user_count)
+        print "Average wages: Male ${0}, Female ${1}, Unknown ${2}, Problem ${3}".format(self.male_average_wage, self.female_average_wage, self.unknown_average_wage, self.problem_average_wage)
 
 myObject = DatabaseAnalyzer()
 myObject.identify_gender()
-
-
-'''
-    for i in z:
-        user_on_page = 0
-        
-        for page in i:
-            worker_id = i[user_on_page]["id"]
-            first_name = i[user_on_page]["name"].split(' ', 1)[0]
-            
-            # Just double checking that there are no duplicates
-            if worker_id in user_ids:
-                duplicate = " and is a duplicate"
-
-            if worker_id not in user_ids:
-                duplicate = ""
-
-            user_count += 1
-            user_on_page += 1
-
-            user_ids.append(worker_id)
-
-            # Using gender_detector package
-            gender_by_detector = detector.guess(first_name)
-
-            worker_list.append([first_name, gender_by_detector])
-
-            print "Number " + str(user_count) + " is {0}".format(first_name) + " with id number {0}".format(worker_id) + "who is {0} (detector)".format(gender_by_detector) + duplicate
-
-
-    # Comparing the effectivness of approaches to identifying gender
-    for i in worker_list:
-        if i[1] == "male":
-            male_detector_count += 1
-        elif i[1] == "female":
-            female_detector_count += 1
-        elif i[1] == "unknown":
-            unknown_detector_count += 1
-
-    detector_rate = (male_detector_count + female_detector_count) / float(male_detector_count + female_detector_count + unknown_detector_count) * 100
-
-
-    print "Using gender_detector package: Male: {0}, Female: {1}, Unknown: {2}. ".format(male_detector_count, female_detector_count, unknown_detector_count) + "This is the rate of identifying gender: {0}%".format(detector_rate)
-'''
-
 
 
 
