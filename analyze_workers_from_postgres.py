6fc086f1653dd337a9d7b951a295666180ba3633
Eureka Foong <eureka.foong@gmail.com>
2017-10-26 14:03:07 -0500

    added std deviation

--- analyze_workers_from_postgres.py
+++ analyze_workers_from_postgres.py
@@ -35,6 +35,9 @@ class DatabaseAnalyzer:
         self.unknown_average_wage = 0
         self.problem_average_wage = 0
         self.no_detailed_info_count = 0
+        self.male_std_dev = 0
+        self.female_std_dev = 0
+        self.unknown_std_dev = 0
 
     def identify_gender(self):
         self.cur.execute("SELECT detailed_info FROM upwork_worldwide_allskills_2017_10_21;")
@@ -79,10 +82,14 @@ class DatabaseAnalyzer:
         self.female_average_wage = numpy.mean(self.female_list)
         self.unknown_average_wage = numpy.mean(self.unknown_list)
         self.problem_average_wage = numpy.mean(self.problem_list)
+        self.male_std_dev = numpy.std(self.male_list)
+        self.female_std_dev = numpy.std(self.female_list)
+        self.unknown_std_dev = numpy.std(self.unknown_list)
 
         print "Using gender_detector package: Male: {0}, Female: {1}, Unknown: {2}, Problem: {3}. ".format(self.male_detector_count, self.female_detector_count, self.unknown_detector_count, self.problem_detector_count) + "This is the rate of identifying gender: {0}%".format(self.detector_rate)
         print "This is our final user count: {0}".format(self.user_count)
         print "Average wages: Male ${0}, Female ${1}, Unknown ${2}, Problem ${3}".format(self.male_average_wage, self.female_average_wage, self.unknown_average_wage, self.problem_average_wage)
+        print "Standard deviation: Male: {0}, Female {1}, Unknown {3}".format(self.male_std_dev, self.female_std_dev, self.unknown_std_dev)
         print "And we had these many profiles with empty detailed_info: {0} or {1}%".format(self.no_detailed_info_count, float(self.no_detailed_info_count/self.user_count)*100)
 myObject = DatabaseAnalyzer()
 myObject.identify_gender()
